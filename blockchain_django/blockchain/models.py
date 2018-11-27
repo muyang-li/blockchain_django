e1bf17617062bcbdf02faf7017c732cc5e111126
muyang_li <631918713@qq.com>
2018-11-27 10:21:22 +0800

    changed models.py
    changed models.py

--- blockchain_django/blockchain/models.py
+++ blockchain_django/blockchain/models.py
@@ -6,6 +6,18 @@ from django.db import models
 class Block(models.Model):
     index = models.IntegerField()
     timestamp = models.TimeField()
-    data = models.TextField()
+    #数据字段部分
+    trackNum = models.CharField(max_length = 64)#运单号
+    deviceId = models.CharField(max_length = 64)
+    deviceName = models.CharField(max_length = 64)
+    dpCreationTime = models.TimeField()
+    location = models.CharField(max_length = 64)
+    image = models.CharField(max_length = 64)#图片文件名
+
     previous_hash = models.CharField(max_length = 64)
     self_hash = models.CharField(max_length = 64)
+
+class User(models.Model):
+    uid = models.CharField(max_length = 13)#身份证号
+    trackNumSet = models.TextField()
+    name = models.CharField(max_length = 32)
